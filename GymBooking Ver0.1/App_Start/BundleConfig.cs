724ccb6323adae39b5084e8c14fd21f55c7ba70d
Dennis1111 <dennis.nilsson1111@gmail.com>
2018-07-17 23:24:35 +0200

    ajax

--- GymBooking Ver0.1/App_Start/BundleConfig.cs
+++ GymBooking Ver0.1/App_Start/BundleConfig.cs
@@ -8,10 +8,18 @@ namespace GymBooking_Ver0._1
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new ScriptBundle("~/bundles/gym").Include(
+                "~/Scripts/jquery-{version}.js",
+                "~/Scripts/jquery-ui-{version}.js",
+                "~/Scripts/jquery.unobtrusive*",
+                "~/Scripts/jquery.validate*"
+                ));
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.unobtrusive*",
                         "~/Scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
